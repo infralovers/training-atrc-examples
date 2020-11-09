9b5b353646954239e0e9ec854d791474e824a678
Jürgen Brüder <juergen.brueder@gmail.com>
2020-11-09 10:59:25 +0100

    simplify

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,13 +2,6 @@ require File.join(File.dirname(__FILE__), '..', '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/dsl'
 
 Capybara.app = Blog
-
-class BlogWorld
-  include Capybara::DSL
-end
-
-World do
-  BlogWorld.new
-end
